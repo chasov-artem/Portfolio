190e5ca3c5c59d7e2ee745499e51b4d596aff6f3
chasov-artem <chasov90@gmail.com>
2025-03-12 23:00:10 +0200

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,50 +1,56 @@
+import styles from "./Footer.module.css";
 import {
   FaGithub,
   FaInstagram,
   FaLinkedin,
   FaTelegramPlane,
 } from "react-icons/fa";
-import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <div>
-      <h2>Contact me:</h2>
-      <div className={styles.socials}>
-        <a
-          href="https://github.com/chasov-artem"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.icon}
-        >
-          <FaGithub />
-        </a>
-        <a
-          href="https://linkedin.com/in/artem-chasov-504a351aa"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.icon}
-        >
-          <FaLinkedin />
-        </a>
-        <a
-          href="https://t.me/chasovartem"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.icon}
-        >
-          <FaTelegramPlane />
-        </a>
-        <a
-          href="https://www.instagram.com/be_real_awesome_instead"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.icon}
-        >
-          <FaInstagram />
-        </a>
+    <div className={styles.contactMe}>
+      <h2 className={styles.title}>Contact me:</h2>
+      <p className={styles.email}>Email: chasov90@gmail.com</p>
+
+      <div className={styles.socialsWrap}>
+        <p className={styles.socialTitle}>Socials:</p>
+        <div className={styles.socials}>
+          <a
+            href="https://github.com/chasov-artem"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.iconLink}
+          >
+            <FaGithub className={styles.icon} />
+          </a>
+          <a
+            href="https://linkedin.com/in/artem-chasov-504a351aa"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.iconLink}
+          >
+            <FaLinkedin className={styles.icon} />
+          </a>
+          <a
+            href="https://t.me/chasovartem"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.iconLink}
+          >
+            <FaTelegramPlane className={styles.icon} />
+          </a>
+          <a
+            href="https://www.instagram.com/be_real_awesome_instead"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.iconLink}
+          >
+            <FaInstagram className={styles.icon} />
+          </a>
+        </div>
       </div>
     </div>
   );
 };
+
 export default Footer;
